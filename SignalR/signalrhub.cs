cb27abf3a5cb92ec9c169f1c44483f9183899e83
saif <saif.iqbal_2007@hotmail.com>
2016-04-12 01:30:41 +0500

    SignalR hub class added to project.

---
+++ SignalR/signalrhub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+    public class signalrhub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}

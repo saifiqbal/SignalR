2634dbb7c081394b6f2d8c4701046b0f356b89dd
saif <saif.iqbal_2007@hotmail.com>
2016-04-13 01:30:47 +0500

    client side function SendServerTime created in timer.js to get servertime using signalR.

---
+++ SignalR/Scripts/timer.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    // Defining a connection to the server hub.
+    var myHub = $.connection.testHub;
+    // Setting logging to true so that we can see whats happening in the browser console log. [OPTIONAL]
+    $.connection.hub.logging = true;
+    // Start the hub
+    $.connection.hub.start();
+
+    // This is the client method which is being called inside the MyHub constructor method every 3 seconds
+    myHub.client.SendServerTime = function (serverTime) {
+        // Set the received serverTime in the span to show in browser
+        $("#newTime").text(serverTime);
+    };
+}());
--- SignalR/TestHub.cs
+++ SignalR/TestHub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace SignalR
 {
@@ -11,6 +12,23 @@ namespace SignalR
         public void Hello()
         {
             Clients.All.hello();
+        }
+        public TestHub()
+        {
+            // Create a Long running task to do an infinite loop which will keep sending the server time
+            // to the clients every 3 seconds.
+            var taskTimer = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    string timeNow = DateTime.Now.ToString();
+                    //Sending the server time to all the connected clients on the client method SendServerTime()
+                    Clients.All.SendServerTime(timeNow);
+                    //Delaying by 3 seconds.
+                    await Task.Delay(3000);
+                }
+            }, TaskCreationOptions.LongRunning
+                );
         }
     }
 }
